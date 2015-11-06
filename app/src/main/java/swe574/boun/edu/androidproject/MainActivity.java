e07b7e4b455d110e84cec085bad43bd4ff19cd03
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-06 21:15:18 +0200

    Drawer listener is updated

--- app/src/main/java/swe574/boun/edu/androidproject/MainActivity.java
+++ app/src/main/java/swe574/boun/edu/androidproject/MainActivity.java
@@ -70,18 +70,13 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Class selectedFragment = null;
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_groups) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_profile) {
 
         }
 

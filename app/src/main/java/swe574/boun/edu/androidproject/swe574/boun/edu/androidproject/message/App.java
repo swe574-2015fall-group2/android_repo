19e18246d3618a5b829a0e4b508aed1e7ad1447d
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-08 12:45:34 +0200

    Message System is fully implemented

---
+++ app/src/main/java/swe574/boun/edu/androidproject/swe574/boun/edu/androidproject/message/App.java
@@ -0,0 +1,29 @@
+package swe574.boun.edu.androidproject.swe574.boun.edu.androidproject.message;
+
+import android.app.Application;
+import android.os.Handler;
+
+import swe574.boun.edu.androidproject.swe574.boun.edu.androidproject.tools.NativeLoader;
+
+public class App extends Application {
+
+    private static App Instance;
+    public static volatile Handler applicationHandler = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Instance=this;
+
+        applicationHandler = new Handler(getInstance().getMainLooper());
+
+        NativeLoader.initNativeLibs(App.getInstance());
+
+    }
+
+    public static App getInstance()
+    {
+        return Instance;
+    }
+}

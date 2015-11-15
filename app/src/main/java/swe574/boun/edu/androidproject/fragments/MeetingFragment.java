9a7e61f464dd5392dfcd8b458778419fc200f24d
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-16 00:03:36 +0200

    User interfaces fro group managent is refined.

--- app/src/main/java/swe574/boun/edu/androidproject/fragments/MeetingFragment.java
+++ app/src/main/java/swe574/boun/edu/androidproject/fragments/MeetingFragment.java
@@ -37,7 +37,7 @@ public class MeetingFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_login, null, false);
+        return inflater.inflate(R.layout.fragment_discussion, null, false);
     }
 
     @Override
--- app/src/main/java/swe574/boun/edu/androidproject/fragments/NoteFragment.java
+++ app/src/main/java/swe574/boun/edu/androidproject/fragments/NoteFragment.java
@@ -38,7 +38,7 @@ public class NoteFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_login, null, false);
+        return inflater.inflate(R.layout.fragment_discussion, null, false);
     }
 
     @Override

368fa9b9fb37660f6a6185eb2fdf7fabc892e0f6
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-16 19:54:39 +0200

    Fragment layouts are updated.

--- app/src/main/java/swe574/boun/edu/androidproject/fragments/MeetingFragment.java
+++ app/src/main/java/swe574/boun/edu/androidproject/fragments/MeetingFragment.java
@@ -37,7 +37,7 @@ public class MeetingFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discussion, null, false);
+        return inflater.inflate(R.layout.fragment_meeting, null, false);
     }
 
     @Override

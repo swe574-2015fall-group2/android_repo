73d0bf365181d435ad55327e951e3b60f71e19e8
Hayri Can Akyel <cakyel@gmail.com>
2015-11-23 03:20:15 +0200

    ViewNoteActivity created

--- app/src/main/java/swe574/boun/edu/androidproject/ViewNoteActivity.java
+++ app/src/main/java/swe574/boun/edu/androidproject/ViewNoteActivity.java
@@ -1,31 +1,45 @@
 package swe574.boun.edu.androidproject;
 
-import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.AttributeSet;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import swe574.boun.edu.androidproject.R;
-import swe574.boun.edu.androidproject.fragments.GroupHomeFragment;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ViewNoteActivity extends AppCompatActivity {
 
+    private ViewNoteTask mNoteTask;
+    private TextView mNoteTags;
+    private TextView mNoteName;
+    private TextView mNoteDesc;
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_note);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_note);
+
+
+        mNoteTask = null;
+    }
+
+    private class ViewNoteTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mNoteName = (TextView) findViewById(R.id.noteName);
+            mNoteDesc = (TextView) findViewById(R.id.noteDesc);
+            mNoteTags = (TextView) findViewById(R.id.noteTags);
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            //TODO Implement webservice
+            return null;
+        }
     }
 }

3fc2a319a1396f38305f58d9552463d9ae59fdcc
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-25 22:30:33 +0200

    All Groups Activity is created

---
+++ app/src/main/java/swe574/boun/edu/androidproject/AllGroupsActivity.java
@@ -0,0 +1,15 @@
+package swe574.boun.edu.androidproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jongaros on 11/25/2015.
+ */
+public class AllGroupsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_groups);
+    }
+}

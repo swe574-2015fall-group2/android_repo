6b1be589f9fdafaeb3abec3edac0330b545f04ed
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-11-26 19:14:05 +0200

    NewGroupActivity and CreateGroupTask are added and fully implemented.

--- app/src/main/java/swe574/boun/edu/androidproject/NewGroupActivity.java
+++ app/src/main/java/swe574/boun/edu/androidproject/NewGroupActivity.java
@@ -5,28 +5,30 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import swe574.boun.edu.androidproject.model.Group;
+import swe574.boun.edu.androidproject.tasks.CreateGroupTask;
 
 
 public class NewGroupActivity extends AppCompatActivity {
     private Button mCreateButton;
+    private String mAuth;
     private EditText mGroupNameView;
     private EditText mGroupDescriptionView;
-    private EditText mGroupTagsView;
     private CreateGroupTask mTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_group);
+        mAuth = savedInstanceState.getString("user");
 
         mGroupNameView = (EditText) findViewById(R.id.groupName);
         mGroupDescriptionView = (EditText) findViewById(R.id.groupDesc);
-        mGroupTagsView = (EditText) findViewById(R.id.groupTags);
 
         mCreateButton = (Button) findViewById(R.id.create_group);
         mCreateButton.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +37,6 @@ public class NewGroupActivity extends AppCompatActivity {
                 attemptCreate();
             }
         });
-
     }
 
     private void attemptCreate() {
@@ -43,54 +44,34 @@ public class NewGroupActivity extends AppCompatActivity {
             return;
         }
 
-        View view = null;
-        String name,description,tags;
+        boolean cancel = false;
+        View focus = null;
+        String name,description;
         if(!validateDescription(description = mGroupDescriptionView.getText().toString())){
-
-        }
-        if(!validateTags(tags = mGroupTagsView.getText().toString())){
-
+            focus = mGroupDescriptionView;
+            cancel = true;
         }
         if (!validateName(name = mGroupNameView.getText().toString())){
-            
+            focus = mGroupNameView;
+            cancel = true;
         }
-        
+
+        if(!cancel){
+            mTask = new CreateGroupTask(this, mAuth, (ViewGroup) ((ViewGroup) this.findViewById(android.R.id.content)).getChildAt(0));
+            mTask.execute((Void) null);
+        }
+        else {
+            focus.requestFocus();
+        }
     }
 
     private boolean validateName(String s) {
-        return false;
+        return s.length() > 4;
     }
 
-    private boolean validateTags(String s) {
-        return false;
-    }
 
     private boolean validateDescription(String s) {
-        return false;
-    }
-
-
-    private class CreateGroupTask extends AsyncTask<Void, Void, Boolean> {
-        private Group mGroup;
-        private Context mContext;
-
-        public CreateGroupTask(Group mGroup, Context mContext) {
-            this.mGroup = mGroup;
-            this.mContext = mContext;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean success) {
-            if(success){
-                Toast.makeText(mContext, "The group has created successfully.", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        }
+        return s.length() > 10;
     }
 
 }

2d1fa8b103509c5a9b097a1495e97d06e92faa7e
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-12-04 22:48:18 +0200

    Leave Group is implemented.

--- app/src/main/java/swe574/boun/edu/androidproject/fragments/GroupTabFragment.java
+++ app/src/main/java/swe574/boun/edu/androidproject/fragments/GroupTabFragment.java
@@ -1,18 +1,24 @@
 package swe574.boun.edu.androidproject.fragments;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import swe574.boun.edu.androidproject.LoginActivity;
 import swe574.boun.edu.androidproject.R;
 import swe574.boun.edu.androidproject.model.ModelFragment;
 import swe574.boun.edu.androidproject.tasks.GetGroupCalendarTask;
+import swe574.boun.edu.androidproject.tasks.LeaveGroupTask;
 
 public class GroupTabFragment extends ModelFragment {
 
@@ -31,6 +37,28 @@ public class GroupTabFragment extends ModelFragment {
         ListView list = (ListView) rootView.findViewById(R.id.group_calendar);
         GetGroupCalendarTask mCalendarTask = new GetGroupCalendarTask(mGroup, mUser, list);
         mCalendarTask.execute();
+
+        Button leaveButton = (Button) rootView.findViewById(R.id.button_leave);
+        leaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(getContext());
+                builder.setTitle("Leave Group");
+                builder.setMessage("Are you sure to leave this group?\n\nAs our terms of usage, our company holds all of the rights of the content" +
+                        " that is posted, used, shared and uploaded content in our website in whatever form of content it has been used. The content owned by our company will be only used for display purposes only." +
+                        " Pressing 'Yes' " +
+                        "means that you agree on our terms of leaving your group. You can regain property of your content in case you rejoin the group in the future.");
+                builder.setPositiveButton("Yes, I agree", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        LeaveGroupTask task = new LeaveGroupTask(getActivity(), mGroup);
+                        task.execute();
+                    }
+                });
+                builder.setCancelable(true);
+                builder.show();
+            }
+        });
 
         return rootView;
     }
---
+++ app/src/main/java/swe574/boun/edu/androidproject/tasks/LeaveGroupTask.java
@@ -0,0 +1,118 @@
+package swe574.boun.edu.androidproject.tasks;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.MalformedJsonException;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import swe574.boun.edu.androidproject.LoginActivity;
+import swe574.boun.edu.androidproject.message.App;
+import swe574.boun.edu.androidproject.model.Group;
+import swe574.boun.edu.androidproject.model.User;
+
+/**
+ * Created by Jongaros on 12/4/2015.
+ */
+public class LeaveGroupTask extends AsyncTask<Void, Void, Boolean> {
+    private Activity mActivity;
+    private Group mGroup;
+
+    public LeaveGroupTask(Activity mActivity, Group mGroup) {
+        this.mActivity = mActivity;
+        this.mGroup = mGroup;
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        try {
+            HttpURLConnection httpURLConnection = null;
+            // Create a new UrlConnection
+            URL postUrl = new URL("http://162.243.215.160:9000/v1/group/leave");
+            // Open the created connection to server.
+            httpURLConnection = (HttpURLConnection) postUrl.openConnection();
+            // Set up the post parameters
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setRequestProperty("Content-Type", "application/json");
+            // Create JSON String
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("authToken", App.mAuth);
+            jsonObject.accumulate("groupId", mGroup.getmID());
+            String json = jsonObject.toString();
+            // Create request output stream.
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            // Create a writer to write on the output stream.
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            // Send the post request
+            writer.write(json);
+            writer.flush();
+            writer.close();
+            outputStream.close();
+            httpURLConnection.connect();
+            // Get response code
+            int response = httpURLConnection.getResponseCode();
+            // Get the Response
+            String responseJson = "";
+            if (response == HttpURLConnection.HTTP_OK) {
+                //Response is okay
+                String line = "";
+                BufferedReader reader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseJson += line;
+                }
+            } else {
+                // Server is down or webserver is changed.
+                throw new IllegalStateException("Response code is not valid");
+            }
+            httpURLConnection.disconnect();
+            JSONObject object = new JSONObject(responseJson);
+            if (object.getString("status").equals("success")) {
+                return true;
+            } else {
+                    throw new MalformedJsonException("Returned JSON String isn't fit the format.");
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean bool) {
+        String message;
+        if (bool) {
+            message = "You have been successfully left the group";
+        } else {
+            message = "There is a problem with the server, please try again later";
+        }
+
+        Toast.makeText(mActivity, message, Toast.LENGTH_LONG);
+        Intent intent = new Intent(mActivity, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        if(bool)
+        mActivity.startActivity(intent);
+    }
+}

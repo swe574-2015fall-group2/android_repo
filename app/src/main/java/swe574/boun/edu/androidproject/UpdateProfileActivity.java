53b6fa151abdfa29c67903bd87da68780a7585ed
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-12-13 16:26:15 +0200

    UpdateProfile Activity - Layout
    -UpdateProfile Activity screen is created.
    -Icons for the menu items are created.

---
+++ app/src/main/java/swe574/boun/edu/androidproject/UpdateProfileActivity.java
@@ -0,0 +1,16 @@
+package swe574.boun.edu.androidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+        setTitle("Update Profile");
+
+
+    }
+}

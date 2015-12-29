0aa089610972fa6f2ccd6b81077e4bd10817ede5
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-12-29 20:11:54 +0200

    View Meeting is Implemented
    *NOT TESTED*
    -The service and ui connection is implemented.
    Fixes #31

--- app/src/main/java/swe574/boun/edu/androidproject/ViewMeetingActivity.java
+++ app/src/main/java/swe574/boun/edu/androidproject/ViewMeetingActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -15,8 +17,13 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import swe574.boun.edu.androidproject.message.App;
+import swe574.boun.edu.androidproject.model.ContactDetails;
 import swe574.boun.edu.androidproject.model.Meeting;
+import swe574.boun.edu.androidproject.model.Tag;
 import swe574.boun.edu.androidproject.model.User;
 import swe574.boun.edu.androidproject.network.JSONRequest;
 
@@ -28,8 +35,9 @@ public class ViewMeetingActivity extends AppCompatActivity {
     private TextView mMeetingDay;
     private TextView mMeetingMonth;
     private TextView mMeetingYear;
-    private TextView mMeetingAgenda;
-    private TextView mMeetingToDo;
+    private ListView mMeetingAgenda;
+    private ListView mMeetingToDo;
+    private TextView mMeetingDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,7 @@ public class ViewMeetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_meeting);
         Intent i = getIntent();
         mUser = i.getParcelableExtra("user");
-        Meeting meeting = i.getParcelableExtra("meeting");
+        final Meeting meeting = i.getParcelableExtra("meeting");
         JSONObject requestObject = new JSONObject();
 
         try {
@@ -47,6 +55,14 @@ public class ViewMeetingActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        mMeetingTags = (TextView) findViewById(R.id.meetingTags);
+        mMeetingLocation = (TextView) findViewById(R.id.MeetingLocation);
+        mMeetingDay = (TextView) findViewById(R.id.MeetingDay);
+        mMeetingMonth = (TextView) findViewById(R.id.MeetingMonth);
+        mMeetingYear = (TextView) findViewById(R.id.MeetingYear);
+        mMeetingAgenda = (ListView) findViewById(R.id.MeetingAgendaListView);
+        mMeetingToDo = (ListView) findViewById(R.id.MeetingTodoListView);
+        mMeetingDetails = (TextView) findViewById(R.id.MeetingContactDetailsTextView);
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         JSONRequest meetingRequest = new JSONRequest("http://162.243.215.160:9000/v1/meeting/get", new Response.Listener<String>() {
             @Override
@@ -57,6 +73,45 @@ public class ViewMeetingActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 setTitle(mMeeting.getmName());
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Tag g : mMeeting.getmTags())
+                    stringBuilder.append(g.getLabel() + ", ");
+                mMeetingTags.setText(stringBuilder.substring(0, stringBuilder.length() - 2).toString());
+                mMeetingLocation.setText(mMeeting.getmLocation());
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(mMeeting.getmDate());
+                mMeetingDay.setText(calendar.get(Calendar.DATE));
+                mMeetingMonth.setText(new SimpleDateFormat("MMM").format(mMeeting.getmDate()));
+                mMeetingYear.setText(calendar.get(Calendar.YEAR));
+                if(mMeeting.getmAgenda() != null) {
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(ViewMeetingActivity.this, android.R.layout.simple_list_item_1, mMeeting.getmAgenda());
+                    mMeetingAgenda.setAdapter(adapter);
+                }
+                if(mMeeting.getmToDo() != null){
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(ViewMeetingActivity.this, android.R.layout.simple_list_item_1, mMeeting.getmToDo());
+                    mMeetingToDo.setAdapter(adapter);
+                }
+                if(mMeeting.getmDetails() != null){
+                    stringBuilder = new StringBuilder();
+                    ContactDetails contactDetails = mMeeting.getmDetails();
+                    if(contactDetails.getmName() != null){
+                        stringBuilder.append(contactDetails.getmName()).append(" ");
+                    }
+                    if(contactDetails.getmSurname() != null){
+                        stringBuilder.append(contactDetails.getmSurname()).append(" ");
+                    }
+                    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                    stringBuilder.append("\n");
+                    if(contactDetails.getmMail() != null){
+                        stringBuilder.append(contactDetails.getmMail()).append(" ");
+                    }
+                    if(contactDetails.getmPhone() != null){
+                        stringBuilder.append(contactDetails.getmPhone()).append(" ");
+                    }
+                    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                    mMeetingDetails.setText(stringBuilder.toString());
+
+                }
             }
         }, new Response.ErrorListener() {
             @Override

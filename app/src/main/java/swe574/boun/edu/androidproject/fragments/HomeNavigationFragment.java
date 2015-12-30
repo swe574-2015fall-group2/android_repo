09070177553601779ddea160b93cafa1f4c41230
Orkun Kocyigit <orkunkocyigit@gmx.com>
2015-12-30 20:52:48 +0200

    Home screen is updated Fixes #15

--- app/src/main/java/swe574/boun/edu/androidproject/fragments/HomeNavigationFragment.java
+++ app/src/main/java/swe574/boun/edu/androidproject/fragments/HomeNavigationFragment.java
@@ -4,17 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 import swe574.boun.edu.androidproject.HomeDrawerActivity;
 import swe574.boun.edu.androidproject.NewGroupActivity;
 import swe574.boun.edu.androidproject.R;
+import swe574.boun.edu.androidproject.adapters.ListMeetingAdapter;
+import swe574.boun.edu.androidproject.message.App;
 import swe574.boun.edu.androidproject.model.HomeFragment;
+import swe574.boun.edu.androidproject.model.Meeting;
+import swe574.boun.edu.androidproject.network.JSONRequest;
 import swe574.boun.edu.androidproject.tasks.FetchMyGroupsTask;
 import swe574.boun.edu.androidproject.tasks.OnTaskCompleted;
 
@@ -28,6 +46,7 @@ import swe574.boun.edu.androidproject.tasks.OnTaskCompleted;
 public class HomeNavigationFragment extends HomeFragment {
     boolean first = true;
     private ListView mMyGroupsListView;
+    private ListView mMyMeetingsListView;
 
     public HomeNavigationFragment() {
         // Required empty public constructor
@@ -60,6 +79,60 @@ public class HomeNavigationFragment extends HomeFragment {
             }
         });
         mTask.execute();
+
+        mMyMeetingsListView = (ListView) view.findViewById(R.id.listViewMyMeetings);
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.accumulate("authToken", App.mAuth);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONRequest jsonRequest = new JSONRequest("http://162.243.18.170:9000/v1/meeting/myMeetings", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                ArrayList<Meeting> meetings = null;
+                try {
+                    JSONObject object = new JSONObject(response);
+                    if (object.has("status")) {
+                        Boolean isSuccessful = Objects.equals(object.getString("status"), "success");
+                        if (isSuccessful) {
+                            if (object.get("result") != null) {
+                                Object o = object.get("result");
+                                if (o instanceof JSONArray) {
+                                    meetings = new ArrayList<>();
+                                    JSONArray array = (JSONArray) o;
+                                    for (int i = 0; i < array.length(); i++) {
+                                        Meeting meeting = Meeting.createFromJSON(array.getJSONObject(i));
+                                        meetings.add(meeting);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (meetings != null) {
+                    if (meetings.size() > 0) {
+                        ListMeetingAdapter adapter = new ListMeetingAdapter(mUser, getContext(), meetings);
+                        mMyMeetingsListView.setAdapter(adapter);
+                    } else {
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.item_nogroups, R.id.textview, new String[]{"No planned meetings are found."});
+                        mMyMeetingsListView.setAdapter(adapter);
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("ERROR", "", error);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.item_nogroups, R.id.textview, new String[]{"No planned meetings are found."});
+                mMyMeetingsListView.setAdapter(adapter);
+
+            }
+        }, jsonObject);
+        requestQueue.add(jsonRequest);
 
         return view;
     }

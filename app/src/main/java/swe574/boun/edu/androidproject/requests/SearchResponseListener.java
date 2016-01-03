94afecad3ad4422c349a1243cdd4ff7e218ba647
Orkun Kocyigit <orkunkocyigit@gmx.com>
2016-01-03 19:08:28 +0200

    SearchResponseListener base is created
    -The search webservice is ready to be implemented.

---
+++ app/src/main/java/swe574/boun/edu/androidproject/requests/SearchResponseListener.java
@@ -0,0 +1,23 @@
+package swe574.boun.edu.androidproject.requests;
+
+import com.android.volley.Response;
+
+import swe574.boun.edu.androidproject.tasks.OnTaskCompleted;
+
+/**
+ * Created by Jongaros on 1/3/2016.
+ */
+public class SearchResponseListener implements Response.Listener<String> {
+    private OnTaskCompleted mListener;
+
+    public SearchResponseListener(OnTaskCompleted mListener) {
+        this.mListener = mListener;
+    }
+
+    @Override
+    public void onResponse(String response) {
+        if(mListener != null){
+            mListener.onTaskCompleted(null);
+        }
+    }
+}

3f33880275bb689c1ebe09c4e938b60eb36be699
Orkun Kocyigit <orkunkocyigit@gmx.com>
2016-01-03 20:16:17 +0200

    SearchAdapter activity linking base method is implemented

--- app/src/main/java/swe574/boun/edu/androidproject/adapters/SearchResultsAdapter.java
+++ app/src/main/java/swe574/boun/edu/androidproject/adapters/SearchResultsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import swe574.boun.edu.androidproject.R;
 import swe574.boun.edu.androidproject.model.SearchResult;
@@ -26,13 +27,31 @@ public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdap
     }
 
     @Override
-    public void onBindViewHolder(SearchResultsAdapter.SearchResultsViewHolder holder, int position) {
-        SearchResult.SearchDetail result = mResult.getResultList().get(position);
+    public void onBindViewHolder(final SearchResultsAdapter.SearchResultsViewHolder holder, int position) {
+        final SearchResult.SearchDetail result = mResult.getResultList().get(position);
         holder.mEntityTypeTextView.setText(result.getType().toString());
         holder.mIdTextView.setText(result.getId());
         holder.mDescriptionTextView.setText(result.getDescription());
         // No SQL injection here ^^
         holder.mTagTextView.setText("#" + result.getTag().getTag() + " (" + result.getTag().getClazz() + ")" );
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (result.getType()){
+                    case GROUP:
+                        break;
+                    case DISCUSSION:
+                        break;
+                    case NOTE:
+                        break;
+                    case MEETING:
+                        break;
+                    case RESOURCE:
+                        Toast.makeText(holder.itemView.getContext(), "This feature is currently in development", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 
     @Override
